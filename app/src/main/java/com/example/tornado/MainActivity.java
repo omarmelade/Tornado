4def9d063f9ccb67e686e20ff86f41751c790a09
o.khial <omarkhial74@gmail.com>
2020-10-09 16:41:07 +0200

    - fix homescreen - add rolling dice and random number generator

--- app/src/main/java/com/example/tornado/MainActivity.java
+++ app/src/main/java/com/example/tornado/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tornado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageButton open_dice = (ImageButton) findViewById(R.id.game_dice);
+        open_dice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RandomDiceActivity.class));
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/tornado/RandomDiceActivity.java
@@ -0,0 +1,41 @@
+package com.example.tornado;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+public class RandomDiceActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_dice);
+
+
+        final TextView dice_res = (TextView) findViewById(R.id.dice_result);
+        final Button btn = (Button) findViewById(R.id.randomize_dice);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dice_res.setText(randomize(6));
+            }
+        });
+    }
+
+    public String randomize(int n){
+        Random r = new Random();
+        int randomnumber = r.nextInt(6);
+        return randomnumber + "";
+    }
+}
