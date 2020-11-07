740e401fca4aa1fb6bf66850e17fd6987c124b3e
omarmelade <omarkhial74@gmail.com>
2020-11-07 16:08:04 +0100

    somes changes, storage optimizations

--- app/src/main/java/com/example/tornado/Activity/JokeAPIActivity.java
+++ app/src/main/java/com/example/tornado/Activity/JokeAPIActivity.java
@@ -84,23 +84,21 @@ public class JokeAPIActivity extends AppCompatActivity {
                     String content = jsonObject.getString("content");
 
                     mTextViewRes.setText(content);
-                    System.out.println(response.toString());
+                    //System.out.println(response.toString());
                 } catch (JSONException e) {
-                    System.out.println(e);
                     e.printStackTrace();
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                System.out.println(error);
                 error.printStackTrace();
             }
         }
         ) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String>  params = new HashMap<String, String>();
+                Map<String, String>  params = new HashMap<>();
                 params.put("Authorization: Bearer", getResources().getString(R.string.tokenJoke));
 
                 return params;
